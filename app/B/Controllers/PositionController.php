32ca854a76d1959f9dc177a26a0063fbd34cc562
刘骏霖 <763606865@qq.com>
2026-05-20 09:59:06 +0800

    feat: 1. 部门、岗位、职工API接口 2. readme

--- app/B/Controllers/PositionController.php
+++ app/B/Controllers/PositionController.php
@@ -3,7 +3,6 @@
 namespace App\B\Controllers;
 
 use App\B\Requests\PositionRequest;
-use App\Exceptions\BadRequestException;
 use App\Models\Employee;
 use App\Models\Position;
 use Illuminate\Database\Eloquent\Builder;
@@ -18,14 +17,12 @@ class PositionController extends Controller
      *
      * GET /b/positions
      *
-     * @param Request $request
-     * @return JsonResponse
      * @throws \Exception
      */
     public function index(Request $request): JsonResponse
     {
         $positions = $this->buildQuery($request)
-            ->paginate((int) $request->input('per_page', 20));
+            ->paginate(max(1, min(100, (int) $request->input('per_page', 20))));
 
         return $this->success($positions);
     }
@@ -35,7 +32,6 @@ class PositionController extends Controller
      *
      * GET /b/positions/create
      *
-     * @return JsonResponse
      * @throws \Exception
      */
     public function create(): JsonResponse
@@ -48,9 +44,6 @@ class PositionController extends Controller
      *
      * POST /b/positions
      *
-     * @param PositionRequest $request
-     * @return JsonResponse
-     * @throws BadRequestException
      * @throws \Exception
      */
     public function store(PositionRequest $request): JsonResponse
@@ -65,8 +58,6 @@ class PositionController extends Controller
      *
      * GET /b/positions/{id}
      *
-     * @param string $id
-     * @return JsonResponse
      * @throws \Exception
      */
     public function show(string $id): JsonResponse
@@ -85,8 +76,6 @@ class PositionController extends Controller
      *
      * GET /b/positions/{id}/edit
      *
-     * @param string $id
-     * @return JsonResponse
      * @throws \Exception
      */
     public function edit(string $id): JsonResponse
@@ -99,9 +88,6 @@ class PositionController extends Controller
      *
      * PUT /b/positions/{id}
      *
-     * @param PositionRequest $request
-     * @param string $id
-     * @return JsonResponse
      * @throws \Exception
      */
     public function update(PositionRequest $request, string $id): JsonResponse
@@ -122,9 +108,6 @@ class PositionController extends Controller
      *
      * DELETE /b/positions/{id}
      *
-     * @param string $id
-     * @return JsonResponse
-     * @throws BadRequestException
      * @throws \Exception
      */
     public function destroy(string $id): JsonResponse
