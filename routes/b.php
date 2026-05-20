a77c5cad3aec40ff4dcd4b732ab0dc0efc375820
刘骏霖 <763606865@qq.com>
2026-05-20 17:11:35 +0800

    fix: 1. 修改套餐功能点及菜单

--- routes/b.php
+++ routes/b.php
@@ -32,20 +32,6 @@ Route::middleware('auth:b')->group(function (): void {
     Route::resource('positions', PositionController::class);
     // ==============================================================================
 
-    // 考勤规则
-    Route::resource('attendance-rules', AttendanceRuleController::class);
-    // ==============================================================================
-
-    // 考勤记录
-    Route::get('/attendance-schedules', [AttendanceScheduleController::class, 'index']);
-    Route::get('/attendance-schedules/export', [AttendanceScheduleController::class, 'export']);
-    Route::get('/attendance-schedules/{id}', [AttendanceScheduleController::class, 'show']);
-    // ==============================================================================
-
-    // 假期类型
-    Route::resource('leave-types', LeaveTypeController::class);
-    // ==============================================================================
-
     // 职工管理
     // 职工管理-列表
     Route::get('/employees', [EmployeeController::class, 'index']);
