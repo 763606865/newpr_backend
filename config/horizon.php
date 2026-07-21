ea68afe598fe2e8c2ea061f931c57983964d26f4
刘骏霖 <763606865@qq.com>
2026-07-21 09:18:18 +0800

    fix: 1. 修复horizon没有测试环境的配置

--- config/horizon.php
+++ config/horizon.php
@@ -220,6 +220,13 @@ return [
                 'balanceCooldown' => 3,
             ],
         ],
+        'develop' => [
+            'supervisor-1' => [
+                'maxProcesses' => 10,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
+        ],
 
         'local' => [
             'supervisor-1' => [
